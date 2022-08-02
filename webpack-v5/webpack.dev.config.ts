33a16aff25bd7dcf198fe4ee98e03ddbc0effe8e
João Menegaz <joao.menegaz@bairesdev.com>
2022-08-02 08:47:18 -0300

    fix config

--- webpack-v5/webpack.dev.config.ts
+++ webpack-v5/webpack.dev.config.ts
@@ -1,5 +1,5 @@
 import path from "path";
-import { Configuration as WebpackConfiguration, HotModuleReplacementPlugin } from "webpack";
+import { Configuration as WebpackConfiguration } from "webpack";
 import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
