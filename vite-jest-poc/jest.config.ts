ac47f56ab07abca468a6ef67cfb2fc49561f832a
martha.syryca <martha.syryca@newsela.com>
2022-08-04 14:21:24 -0400

    adds vite-jest-poc, renames vite-vitest-poc, updates readme

---
+++ vite-jest-poc/jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+    testEnvironment: "jsdom",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    moduleNameMapper: {
+        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/fileMock.js"
+    },
+}
